fb99f6f77747216e4146ae51c97564a646e28ce2
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-05-08 16:27:17 +0200

    Removing compiled files

--- src/quotebot/Application.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AutomaticQuoteBot_1 = require("./AutomaticQuoteBot");
-const bot = new AutomaticQuoteBot_1.AutomaticQuoteBot();
-bot.sendAllQuotes("FAST");
